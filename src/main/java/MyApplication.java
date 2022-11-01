82042caa543e2bd3134d11408dc3c8de479a37d8
Kai Presler-Marshall <kpresle@ncsu.edu>
2022-11-01 12:53:01 -0400

    Add additional endpoints for REST requests

--- src/main/java/MyApplication.java
+++ src/main/java/MyApplication.java
@@ -2,8 +2,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @EnableAutoConfiguration
@@ -19,8 +22,25 @@ public class MyApplication {
 	    return String.format("Hello, %s", user);	
 	}
 	
-	
+	@GetMapping("/orders")
+	public String getOrders(@RequestParam(required = false) Boolean shipped, @RequestParam(required=false) String after){
+		String response = "You are requesting all ";
 
+		response += null == shipped ? "" : (shipped ? "shipped " : "unshipped ");
+		
+		response += "orders ";
+
+		response += (null == after ? "" : ("that were ordered after " + after));
+		
+		return response;
+		
+	}
+	
+	@PostMapping("/orders")
+	public String createOrder(@RequestBody String newOrder){
+		return String.format("Your new order is %s", newOrder);
+	}
+	
     public static void main(String[] args) {
         SpringApplication.run(MyApplication.class, args);
     }
